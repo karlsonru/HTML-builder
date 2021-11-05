232c63e878691bc3df5898a79c12985866701682
karlsonru <ivpolovnikov@hotmail.com>
2021-11-05 18:40:46 +0300

    feat: add solution task 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,26 @@
+// Данные должны быть выведены в формате <имя файла>-<расширение файла>-<вес файла>. Пример: example - txt - 128.369kb
+
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path')
+
+const secretFolder = path.join(__dirname, './secret-folder');
+
+async function readDir() {
+    const files = await fsPromises.readdir(secretFolder, {withFileTypes: true});
+    for (const file of files) {
+        if (!file.isFile()) continue;
+        
+        filePath = path.join(secretFolder, file.name);        
+        const obj = path.parse(filePath)
+        const stat = await fsPromises.stat(filePath, function(err, stat) {
+            return stat; 
+        })
+
+        const sizeKb = (stat.size / 1024).toFixed(3);
+
+        console.log(`${obj.name} - ${obj.ext.replace('.', '')} - ${sizeKb}kb`)
+    }
+}
+
+readDir()
